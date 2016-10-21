7b9755ca03c213ef28569c04b4e66e6721bbcd9f
Akiko Okabe <aki0u0@gmail.com>
2016-10-21 09:33:52 -0600

    add currency function

---
+++ src/js/components/main/main.filter.js
@@ -0,0 +1,20 @@
+(function() {
+
+  'use strict';
+
+  angular
+    .module('ShoppingCartApp.components.main')
+    .filter('currencyFn', currencyFn);
+
+  currencyFn.$inject = [];
+
+  function currencyFn() {
+    /*jshint validthis: true */
+    return function (input) {
+      let num = input.toString();
+      let position = num.length-2;
+      let output = [num.slice(0, position), '.', num.slice(position)].join('');
+      return output;
+    };
+  }
+})();
--- src/js/components/main/products.service.js
+++ src/js/components/main/products.service.js
@@ -17,7 +17,7 @@
         ingredients: 'concentrated gluten, jewelry, dill, beetle nut, toast',
         caffeineScale: 244,
         price: 1540,
-        inStock: true,
+        inStock: false,
         rating: 1,
         imageUrl: 'http://s7d5.scene7.com/is/image/Teavana/32664_d?$cimg$',
         categories: ['dark', 'cold']
